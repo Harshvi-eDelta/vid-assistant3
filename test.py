cf6ac8ef577a959e14c01180aa3f23eefaa2ef79
Harshvi-eDelta <harshvi.patel@edeltaes.com>
2025-05-01 12:49:23 +0530

    add data augmantation at trainnig time

--- test.py
+++ test.py
@@ -70,10 +70,10 @@ model.to(device)
 model.eval()
 
 # Load custom image (Ensure this image exists in your directory)
-image_path = "/Users/edelta076/Desktop/Project_VID_Assistant3/face_images/fimg27.jpg"         # 3,4,5,7,8,12,13,14,15,18,19,041,048,060,23,24,27       
-original_img = cv2.imread(image_path)                                                           # 1,14,_1,4,5,7,9,11,12,13,14,16,17,18,19,24,25,_1,041,044,0133,27
-                                                                                                # _1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,23,24,25,871,041,042,  044,060,801,27
-if original_img is None:
+image_path = "/Users/edelta076/Desktop/Project_VID_Assistant3/face_images/image00801.jpg"         # 3,4,5,7,8,12,13,14,15,18,19,041,048,060,23,24,27,28,29       
+original_img = cv2.imread(image_path)                                                        # 1,14,_1,4,5,7,9,11,12,13,14,16,17,18,19,24,25,_1,041,044,0133,27,28
+                                                                                             # _1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,23,24,25,871,041,042,  044,060,801,27,28
+if original_img is None:                                                                     # 1,2,4,5,7,8,10,11,12,13,14,15,16,17,18,19,23,24,27,28,29,_1,41,42,44
     raise FileNotFoundError(f"Image not found at path: {image_path}")
 
 # Convert BGR (OpenCV) to RGB
@@ -86,7 +86,7 @@ resized_img = cv2.resize(original_img, (256, 256))
 pil_img = Image.fromarray(original_img)
 
 # Apply transform (resize to 256x256 and normalization)
-transform = get_transforms()
+transform = get_transforms(train=False)
 input_tensor = transform(pil_img).unsqueeze(0).to(device)
 
 # Predict landmarks
